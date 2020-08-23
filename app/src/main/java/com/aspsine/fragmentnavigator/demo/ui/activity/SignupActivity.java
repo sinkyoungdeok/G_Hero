85fb507198f1ce6ecff58575e07ab047df76aafb
sinkyoungdeok <tlsrudejr123@nate.com>
2020-08-24 04:00:37 +0900

    [FIX] 회원가입 기능(파이어베이스 Auth) 완성

--- app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/activity/SignupActivity.java
+++ app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/activity/SignupActivity.java
@@ -1,16 +1,63 @@
 package com.aspsine.fragmentnavigator.demo.ui.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.aspsine.fragmentnavigator.demo.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
+
+    EditText et_email, et_passwd;
+    Button signup_button;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        et_email = (EditText) findViewById(R.id.et_email);
+        et_passwd = (EditText) findViewById(R.id.et_password);
+        signup_button = (Button) findViewById(R.id.signup_button);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+
+        signup_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String email = et_email.getText().toString().trim();
+                String pwd = et_passwd.getText().toString().trim();
+                firebaseAuth.createUserWithEmailAndPassword(email, pwd)
+                        .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if(task.isSuccessful()) {
+                                    Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }else{
+                                    Toast.makeText(SignupActivity.this, "등록 에러", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                            }
+                        });
+
+            }
+        });
+
+
     }
 }
