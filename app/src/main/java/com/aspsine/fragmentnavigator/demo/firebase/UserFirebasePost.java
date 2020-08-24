904c104d4cb82d28b3206f6f7bb19e84f8f954e6
sinkyoungdeok <tlsrudejr123@nate.com>
2020-08-25 04:59:31 +0900

    [FIX] 유저 정보 파이어베이스 클래스 생성

---
+++ app/src/main/java/com/aspsine/fragmentnavigator/demo/firebase/UserFirebasePost.java
@@ -0,0 +1,44 @@
+package com.aspsine.fragmentnavigator.demo.firebase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserFirebasePost {
+    public String id;
+    public String otherHalf;
+    public String name;
+    public String birthday;
+    public String firstDay;
+    public String profileUrl;
+    public String code;
+    public String phoneNumber;
+    public String firstEnrolled;
+
+    public UserFirebasePost(){
+
+    }
+    public UserFirebasePost(String id, String otherHalf,String name, String birthday, String firstDay,String profileUrl, String code, String phoneNumber, String firstEnrolled){
+        this.id = id;
+        this.otherHalf = otherHalf;
+        this.name = name;
+        this.birthday = birthday;
+        this.firstDay = firstDay;
+        this.profileUrl = profileUrl;
+        this.code = code;
+        this.phoneNumber = phoneNumber;
+        this.firstEnrolled = firstEnrolled;
+    }
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("id",id);
+        result.put("otherHalf",otherHalf);
+        result.put("name",name);
+        result.put("birthDay",birthday);
+        result.put("firstDay",firstDay);
+        result.put("profileUrl",profileUrl);
+        result.put("code",code);
+        result.put("phoneNumber",phoneNumber);
+        result.put("firstEnrolled",firstEnrolled);
+        return result;
+    }
+}
