d51ee722efba3ee79b7d2550700ff37f573f5199
sinkyoungdeok <tlsrudejr123@nate.com>
2020-08-25 09:38:09 +0900

    [FIX] 로그인할때 매칭이 안됐을 경우 매칭화면으로 넘김

--- app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/activity/LoginActivity.java
+++ app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/activity/LoginActivity.java
@@ -21,6 +21,7 @@ import android.widget.Toast;
 
 import com.aspsine.fragmentnavigator.demo.R;
 import com.aspsine.fragmentnavigator.demo.broadcast.BroadcastManager;
+import com.aspsine.fragmentnavigator.demo.firebase.CodeFirebasePost;
 import com.aspsine.fragmentnavigator.demo.kakao.SessionCallback;
 import com.aspsine.fragmentnavigator.demo.utils.SharedPrefUtils;
 import com.google.android.gms.auth.api.Auth;
@@ -39,6 +40,11 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
 import com.kakao.util.exception.KakaoException;
@@ -52,6 +58,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private EditText etEmail;
     private EditText etPassword;
     private Button signupBtn;
+    private DatabaseReference mPostReference;
+    private boolean codeCheck = false;
     /* 카카오 로그인 */
     private SessionCallback sessionCallback;
     /* 카카오 로그인 */
@@ -72,6 +80,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         signupBtn = (Button) findViewById(R.id.signup_button);
         button.setOnClickListener(this);
         auth = FirebaseAuth.getInstance();
+        mPostReference = FirebaseDatabase.getInstance().getReference();
 
         /*카카오 로그인*/
         sessionCallback = new SessionCallback();
@@ -124,9 +133,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
     void tryLogin(){
-        String email = String.valueOf(etEmail.getText()).trim();
+        final String email = String.valueOf(etEmail.getText()).trim();
         String password = String.valueOf(etPassword.getText()).trim();
-
+        getFirebaseDatabase(email);
         if(check(email, password)){
             auth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
@@ -136,7 +145,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                                 markUserLogin();
                                 notifyUserLogin();
                                 finish();
-                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                if(codeCheck)
+                                    startActivity(new Intent(LoginActivity.this, ConnectActivity.class));
+                                else
+                                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
                             } else {
                                 Toast.makeText(LoginActivity.this, "로그인 오류", Toast.LENGTH_SHORT).show();
                             }
@@ -144,6 +156,29 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     });
 
         }
+    }
+    public void getFirebaseDatabase(final String id){
+        final ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.d("onDataChange", "Data is Updated");
+
+
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    String key = postSnapshot.getKey();
+                    CodeFirebasePost get = postSnapshot.getValue(CodeFirebasePost.class);
+                    String[] info = {get.id,get.code};
+                    if(info[0].equals(id)) codeCheck = true;
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        mPostReference.child("/code_list/").addValueEventListener(postListener);
     }
     boolean check(String email, String password){
         if (TextUtils.isEmpty(email)){
