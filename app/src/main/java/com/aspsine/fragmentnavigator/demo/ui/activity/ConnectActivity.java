b04e7d83e36b32ceaa5062bfc34b4b41b6178f91
sinkyoungdeok <tlsrudejr123@nate.com>
2020-08-27 06:52:33 +0900

    [FIX] 커플매칭 기능 완성

--- app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/activity/ConnectActivity.java
+++ app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/activity/ConnectActivity.java
@@ -5,24 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.aspsine.fragmentnavigator.demo.R;
 import com.aspsine.fragmentnavigator.demo.firebase.CodeFirebasePost;
+import com.aspsine.fragmentnavigator.demo.firebase.UserFirebasePost;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConnectActivity extends AppCompatActivity {
-    EditText myCode, yourCode;
+    EditText myCode, yourCodeEdt;
     String id;
     String code;
+    String yourId;
     Button btn;
+    boolean btncheck = false;
     private DatabaseReference mPostReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,28 +40,28 @@ public class ConnectActivity extends AppCompatActivity {
         mPostReference = FirebaseDatabase.getInstance().getReference();
 
         myCode = (EditText) findViewById(R.id.myCode);
-        yourCode = (EditText) findViewById(R.id.yourCode);
+        yourCodeEdt = (EditText) findViewById(R.id.yourCode);
         btn = (Button) findViewById(R.id.button);
         myCode.setFocusable(false);
         myCode.setClickable(false);
         Intent intent = getIntent();
         id = intent.getExtras().getString("id");
-        getFirebaseDatabase(id);
+        getFirebaseDatabase();
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(ConnectActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
+                btncheck = true;
+                getFirebaseDatabaseConnect(yourCodeEdt.getText().toString());
             }
         });
 
     }
-    public void getFirebaseDatabase(final String id){
+    public void getFirebaseDatabase(){
         final ValueEventListener postListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 Log.d("onDataChange", "Data is Updated");
+                code = "";
                 for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
                     String key = postSnapshot.getKey();
                     String get = (String) postSnapshot.getValue();
@@ -61,6 +70,11 @@ public class ConnectActivity extends AppCompatActivity {
                         myCode.setText(code);
                     }
 
+                }
+                if(code == "") {
+                    Intent intent = new Intent(ConnectActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
                 }
             }
 
@@ -71,4 +85,88 @@ public class ConnectActivity extends AppCompatActivity {
         };
         mPostReference.child("/code_list/id"+id.replace(".","")).addValueEventListener(postListener);
     }
+    public void getFirebaseDatabaseConnect(final String yourCode){
+        final ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(btncheck) {
+                    boolean yourExistCheck = false;
+                    for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                        String key = postSnapshot.getKey();
+                        CodeFirebasePost get = postSnapshot.getValue(CodeFirebasePost.class);
+                        String[] info = {get.id,get.code};
+                        if(info[1].equals(yourCode)){
+                            yourExistCheck = true;
+                            yourId = info[0];
+                        }
+                    }
+                    if(yourExistCheck) {
+                        codeDeleteFirebaseDatabase(myCode.getText().toString());
+                        codeDeleteFirebaseDatabase(yourCode);
+                        userUpdateFirebaseDatabase(id,yourId,"T");
+                        userUpdateFirebaseDatabase(yourId,id,"F");
+                        Intent intent = new Intent(ConnectActivity.this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(getApplicationContext(),"코드가 존재하지 않습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                    btncheck = false;
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        mPostReference.child("/code_list").addValueEventListener(postListener);
+
+
+    }
+    public void codeDeleteFirebaseDatabase(String delCode) {
+        Query deleteQuery = mPostReference.child("/code_list").orderByChild("code").equalTo(delCode);
+
+        deleteQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    postSnapshot.getRef().removeValue();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+    public void userUpdateFirebaseDatabase(String updateId, final String otherHalf, final String firstEnrolled){
+        Query getQuery = mPostReference.child("/user_list").orderByChild("id").equalTo(updateId);
+
+        getQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    UserFirebasePost user = postSnapshot.getValue(UserFirebasePost.class);
+                    user.otherHalf = otherHalf;
+                    user.firstEnrolled = firstEnrolled;
+                    Map<String, Object> childUpdates = new HashMap<>();
+                    Map<String, Object> postValues = user.toMap();
+                    String temp =  user.id.replace(".","");
+                    childUpdates.put("/user_list/id" +temp , postValues);
+                    mPostReference.updateChildren(childUpdates);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
 }
