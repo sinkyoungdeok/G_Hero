4e5e35cc614af56ffa488ae0e38e0718bcf995a0
sinkyoungdeok <tlsrudejr123@nate.com>
2020-09-01 14:44:48 +0900

    [FIX] 캘린더 일정추가 액티비티에 커스텀 액션바 적용, 취소버튼 누를시에 이전화면으로 돌아가게 해주기

--- app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/activity/AddCalenderActivity.java
+++ app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/activity/AddCalenderActivity.java
@@ -1,16 +1,51 @@
 package com.aspsine.fragmentnavigator.demo.ui.activity;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AdaptiveIconDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.aspsine.fragmentnavigator.demo.R;
 
-public class AddCalenderActivity extends AppCompatActivity {
+import java.util.concurrent.Executors;
 
+public class AddCalenderActivity extends AppCompatActivity {
+    TextView cancel, add,title;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_calender);
+
+        ActionBar actionBar = this.getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);            //액션바 아이콘을 업 네비게이션 형태로 표시합니다.
+        actionBar.setDisplayShowTitleEnabled(false);        //액션바에 표시되는 제목의 표시유무를 설정합니다.
+        actionBar.setDisplayShowHomeEnabled(false);            //홈 아이콘을 숨김처리합니다.
+        actionBar.setDisplayHomeAsUpEnabled(false);
+
+        View actionbar = LayoutInflater.from(this).inflate(R.layout.custom_actionbar, null);
+        actionBar.setCustomView(actionbar);
+
+        cancel = (TextView)findViewById(R.id.cancel);
+        add = (TextView)findViewById(R.id.add);
+        title = (TextView)findViewById(R.id.title);
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+
     }
 }
