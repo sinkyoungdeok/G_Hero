16b0fd7d4e5e66d7edb0e3daf907e7235173761d
sinkyoungdeok <tlsrudejr123@nate.com>
2020-09-04 08:08:28 +0900

    [FIX] (캘린더 일정추가하는 화면) 날짜 선택 기능 추가

--- app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/activity/AddCalenderActivity.java
+++ app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/activity/AddCalenderActivity.java
@@ -9,21 +9,51 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.aspsine.fragmentnavigator.demo.R;
+import com.github.jjobes.slidedatetimepicker.SlideDateTimeListener;
+import com.github.jjobes.slidedatetimepicker.SlideDateTimePicker;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.Executors;
+
 
 public class AddCalenderActivity extends AppCompatActivity {
     TextView cancel, add,title;
+
+    private SimpleDateFormat mFormatter = new SimpleDateFormat("MMMM dd yyyy hh:mm aa");
+    private Button mButton;
+
+    private SlideDateTimeListener listener = new SlideDateTimeListener() {
+
+        @Override
+        public void onDateTimeSet(Date date)
+        {
+            Toast.makeText(AddCalenderActivity.this,
+                    mFormatter.format(date), Toast.LENGTH_SHORT).show();
+        }
+
+        // Optional cancel listener
+        @Override
+        public void onDateTimeCancel()
+        {
+            Toast.makeText(AddCalenderActivity.this,
+                    "Canceled", Toast.LENGTH_SHORT).show();
+        }
+    };
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_calender);
+        mButton = (Button) findViewById(R.id.button2);
 
         ActionBar actionBar = this.getSupportActionBar();
         actionBar.setDisplayShowCustomEnabled(true);
@@ -43,6 +73,24 @@ public class AddCalenderActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v)
+            {
+                new SlideDateTimePicker.Builder(getSupportFragmentManager())
+                        .setListener(listener)
+                        .setInitialDate(new Date())
+                        //.setMinDate(minDate)
+                        //.setMaxDate(maxDate)
+                        //.setIs24HourTime(true)
+                        //.setTheme(SlideDateTimePicker.HOLO_DARK)
+                        //.setIndicatorColor(Color.parseColor("#990000"))
+                        .build()
+                        .show();
             }
         });
 
