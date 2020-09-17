1c7fd2c583e5741b0163adde400a38fc4ee43b9c
sinkyoungdeok <tlsrudejr123@nate.com>
2020-09-17 16:05:28 +0900

    [FIX] (홈 화면) 내이름 보여주기

--- app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/adapter/demo/ui/fragment/MainFragment.java
+++ app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/adapter/demo/ui/fragment/MainFragment.java
@@ -12,13 +12,23 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.aspsine.fragmentnavigator.demo.R;
+import com.aspsine.fragmentnavigator.demo.firebase.UserFirebasePost;
 import com.aspsine.fragmentnavigator.demo.ui.adapter.demo.utils.SharedPrefUtils;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.lang.ref.WeakReference;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,21 +38,16 @@ public class MainFragment extends Fragment {
     public static final String TAG = MainFragment.class.getSimpleName();
 
     public static final String EXTRA_TEXT = "extra_text";
-
-    private static final int MOCK_LOAD_DATA_DELAYED_TIME = 2000;
-
-    private String mText;
+    private static String mText;
 
     private TextView myNameText;
     private TextView yourNameText;
 
-    private ProgressBar progressBar;
+    private DatabaseReference mPostReference;
 
     public static Fragment newInstance(String text) {
         MainFragment fragment = new MainFragment();
-        Bundle bundle = new Bundle();
-        bundle.putString(EXTRA_TEXT, text);
-        fragment.setArguments(bundle);
+        mText = text;
         return fragment;
     }
 
@@ -53,7 +58,7 @@ public class MainFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mText = getArguments().getString(EXTRA_TEXT);
+        //mText = getArguments().getString(EXTRA_TEXT);
     }
 
     @Override
@@ -62,9 +67,10 @@ public class MainFragment extends Fragment {
         // Inflate the layout for this fragment
         //Toast.makeText(getActivity(),"aa",Toast.LENGTH_SHORT).show();
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+        mPostReference = FirebaseDatabase.getInstance().getReference();
         myNameText = (TextView) view.findViewById(R.id.myName);
         yourNameText = (TextView) view.findViewById(R.id.yourName);
-
+        getUserFirebaseDatabase(mText);
         return view;
     }
 
@@ -75,10 +81,26 @@ public class MainFragment extends Fragment {
 
 
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(EXTRA_TEXT, mText);
+
+    public void getUserFirebaseDatabase(String UserId){
+        Query myGetQuery = mPostReference.child("/user_list").orderByChild("id").equalTo(UserId);
+
+        myGetQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    UserFirebasePost user = postSnapshot.getValue(UserFirebasePost.class);
+                    myNameText.setText(user.name);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 
 
