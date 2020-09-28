93c82782999874b783480c6d2ce0ad30f5ddcfae
sinkyoungdeok <tlsrudejr123@nate.com>
2020-09-28 17:50:11 +0900

    [FIX] (캘린더 화면) 커스텀 액션바 적용

--- app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/fragment/CalenderFragment.java
+++ app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/fragment/CalenderFragment.java
@@ -1,5 +1,6 @@
 package com.aspsine.fragmentnavigator.demo.ui.fragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
@@ -241,6 +242,17 @@ public class CalenderFragment extends Fragment  implements BottomNavigatorView.O
     }
     @Override
     public void onPrepareOptionsMenu(Menu menu) {
+        ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);            //액션바 아이콘을 업 네비게이션 형태로 표시합니다.
+        actionBar.setDisplayShowTitleEnabled(false);        //액션바에 표시되는 제목의 표시유무를 설정합니다.
+        actionBar.setDisplayShowHomeEnabled(false);            //홈 아이콘을 숨김처리합니다.
+        actionBar.setDisplayHomeAsUpEnabled(false);
+
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View actionbar = inflater.inflate(R.layout.calender_actionbar, null);
+        actionBar.setCustomView(actionbar);
+
         menu.findItem(R.id.action_logout).setVisible(false);
         menu.findItem(R.id.action_exception).setVisible(false);
         menu.findItem(R.id.action_add).setVisible(true);
