9be92857fd99038eef898f1f2d96f52fc645b40a
sinkyoungdeok <tlsrudejr123@nate.com>
2020-09-28 17:58:14 +0900

    [FIX] (이모티콘 화면) 액션바 커스텀 추가

--- app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/fragment/EmoticonFragment.java
+++ app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/fragment/EmoticonFragment.java
@@ -1,21 +1,26 @@
 package com.aspsine.fragmentnavigator.demo.ui.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.aspsine.fragmentnavigator.demo.R;
+import com.aspsine.fragmentnavigator.demo.ui.widget.BottomNavigatorView;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link EmoticonFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class EmoticonFragment extends Fragment {
+public class EmoticonFragment extends Fragment implements BottomNavigatorView.OnBottomNavigatorViewItemClickListener  {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,20 +34,12 @@ public class EmoticonFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EmoticonFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
-    public static EmoticonFragment newInstance(String param1, String param2) {
-        EmoticonFragment fragment = new EmoticonFragment();
+    public static DdayFragment newInstance(String param1) {
+        DdayFragment fragment = new DdayFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -60,6 +57,35 @@ public class EmoticonFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_emoticon, container, false);
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.fragment_dday, container, false);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getActivity().getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);            //액션바 아이콘을 업 네비게이션 형태로 표시합니다.
+        actionBar.setDisplayShowTitleEnabled(false);        //액션바에 표시되는 제목의 표시유무를 설정합니다.
+        actionBar.setDisplayShowHomeEnabled(false);            //홈 아이콘을 숨김처리합니다.
+        actionBar.setDisplayHomeAsUpEnabled(false);
+
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View actionbar = inflater.inflate(R.layout.emoticon_actionbar, null);
+        actionBar.setCustomView(actionbar);
+
+        menu.findItem(R.id.action_logout).setVisible(false);
+        menu.findItem(R.id.action_exception).setVisible(false);
+        menu.findItem(R.id.action_add).setVisible(true);
+        super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public void onBottomNavigatorViewItemClick(int position, View view) {
+
     }
 }
