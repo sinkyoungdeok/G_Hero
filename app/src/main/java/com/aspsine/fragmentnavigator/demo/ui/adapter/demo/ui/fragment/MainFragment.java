a3c103de4bcd116a3a6a939935666a0bec2c143c
sinkyoungdeok <tlsrudejr123@nate.com>
2020-10-03 08:44:22 +0900

    [FIX] (홈 화면) 내프로필, 상대방프로필 보여주기

--- app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/adapter/demo/ui/fragment/MainFragment.java
+++ app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/adapter/demo/ui/fragment/MainFragment.java
@@ -2,6 +2,7 @@ package com.aspsine.fragmentnavigator.demo.ui.adapter.demo.ui.fragment;
 
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,6 +12,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +22,7 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -29,12 +32,19 @@ import com.aspsine.fragmentnavigator.demo.listener.OnBackPressedListener;
 import com.aspsine.fragmentnavigator.demo.ui.activity.MainActivity;
 import com.aspsine.fragmentnavigator.demo.ui.adapter.demo.utils.SharedPrefUtils;
 import com.aspsine.fragmentnavigator.demo.ui.widget.BottomNavigatorView;
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.lang.ref.WeakReference;
 import java.text.SimpleDateFormat;
@@ -52,7 +62,7 @@ public class MainFragment extends Fragment implements BottomNavigatorView.OnBott
     public static final String TAG = MainFragment.class.getSimpleName();
 
     public static final String EXTRA_TEXT = "extra_text";
-    private static String mText;
+    private static String ID;
 
     private TextView myNameText;
     private TextView yourNameText;
@@ -64,10 +74,18 @@ public class MainFragment extends Fragment implements BottomNavigatorView.OnBott
     MainActivity activity;
     Toast toast;
     long backKeyPressedTime;
+    private String yourID;
+
+    /* profile */
+    private ImageView myImg, yourImg;
+    private FirebaseStorage storage, yourstorage;
+    private StorageReference storageReference, yourstorageReference;
+    private StorageReference pathReference, yourpathReference;
+    /* profile */
 
     public static Fragment newInstance(String text) {
         MainFragment fragment = new MainFragment();
-        mText = text;
+        ID = text;
         return fragment;
     }
 
@@ -95,6 +113,10 @@ public class MainFragment extends Fragment implements BottomNavigatorView.OnBott
         yourNameText = (TextView) view.findViewById(R.id.yourName);
         todayText = (TextView) view.findViewById(R.id.today);
         ingdayText = (TextView) view.findViewById(R.id.ingday);
+        myImg = (ImageView) view.findViewById(R.id.myImg);
+        yourImg = (ImageView) view.findViewById(R.id.yourImg);
+
+
 
         /* 커스텀 액션바 */
         ActionBar actionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
@@ -108,7 +130,7 @@ public class MainFragment extends Fragment implements BottomNavigatorView.OnBott
         actionBar.setCustomView(actionbar);
         /* 커스텀 액션바 */
 
-        getUserFirebaseDatabase(mText);
+        getUserFirebaseDatabase(ID);
 
         SimpleDateFormat mFormatter = new SimpleDateFormat("yyyy년 M월 d일 ");
         Date time = new Date();
@@ -119,6 +141,38 @@ public class MainFragment extends Fragment implements BottomNavigatorView.OnBott
         todayStr += week[oCalendar.get(Calendar.DAY_OF_WEEK) - 1] + "요일";
         todayText.setText(todayStr);
 
+
+        /*profile*/
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReferenceFromUrl("gs://g-hero.appspot.com");
+        pathReference = storageReference.child("images/" + ID+ "Profile.png");
+        pathReference.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if(task.isSuccessful()) {
+                    Glide.with(getContext())
+                            .load(task.getResult())
+                            .into(myImg);
+                    myImg.setBackgroundResource(0);
+                }
+            }
+        });
+
+        yourstorage = FirebaseStorage.getInstance();
+        yourstorageReference = yourstorage.getReferenceFromUrl("gs://g-hero.appspot.com");
+        yourpathReference = yourstorageReference.child("images/" + yourID+ "Profile.png");
+        yourpathReference.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if(task.isSuccessful()) {
+                    Glide.with(getContext())
+                            .load(task.getResult())
+                            .into(yourImg);
+                    yourImg.setBackgroundResource(0);
+                }
+            }
+        });
+        /*profile*/
 
 
         return view;
@@ -158,6 +212,7 @@ public class MainFragment extends Fragment implements BottomNavigatorView.OnBott
                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                             for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
                                 UserFirebasePost yourUser = postSnapshot.getValue(UserFirebasePost.class);
+                                yourID = yourUser.id;
                                 yourNameText.setText(yourUser.name);
 
                             }
