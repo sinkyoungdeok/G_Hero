2bda70cbf1dff92eefe09141ccfbfcee675aaee3
sinkyoungdeok <tlsrudejr123@nate.com>
2020-11-01 16:14:40 +0900

    [FIX] (디데이 추가 화면) 디데이 날짜 눌렀을때 날짜 선택할 수 있게 구성

--- app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/activity/AddDdayActivity.java
+++ app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/activity/AddDdayActivity.java
@@ -3,20 +3,62 @@ package com.aspsine.fragmentnavigator.demo.ui.activity;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.aspsine.fragmentnavigator.demo.R;
+import com.github.jjobes.slidedatetimepicker.SlideDateTimeListener;
+import com.github.jjobes.slidedatetimepicker.SlideDateTimePicker;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AddDdayActivity extends AppCompatActivity {
     private TextView cancel, add,title;
+    private TextView startShow;
+    private String startDate;
+    private SimpleDateFormat mFormatter = new SimpleDateFormat("yyyy. M. d");
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy,M,d");
+    private EditText titleEdit;
+    private DatabaseReference mPostReference;
+    private String ID;
+
+    private SlideDateTimeListener startListener = new SlideDateTimeListener() {
+
+        @Override
+        public void onDateTimeSet(Date date)
+        {
+            startShow.setText(mFormatter.format(date));
+            startDate = dateFormat.format(date).toString();
+        }
+
+        @Override
+        public void onDateTimeCancel()
+        {
+            Toast.makeText(AddDdayActivity.this,
+                    "Canceled", Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_dday);
+        startShow = (TextView) findViewById(R.id.startShow);
+        Date time = new Date();
+        startShow.setText(mFormatter.format(time).toString());
+        startDate = dateFormat.format(time).toString();
+        titleEdit = (EditText) findViewById(R.id.titleEdit);
+        mPostReference = FirebaseDatabase.getInstance().getReference();
+        Intent intent = getIntent();
+        ID = intent.getExtras().getString("id").replace(".","");
 
         ActionBar actionBar = this.getSupportActionBar();
         actionBar.setDisplayShowCustomEnabled(true);
@@ -34,6 +76,24 @@ public class AddDdayActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        startShow.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v)
+            {
+                new SlideDateTimePicker.Builder(getSupportFragmentManager())
+                        .setListener(startListener)
+                        .setInitialDate(new Date())
+                        //.setMinDate(minDate)
+                        //.setMaxDate(maxDate)
+                        //.setIs24HourTime(true)
+                        //.setTheme(SlideDateTimePicker.HOLO_DARK)
+                        //.setIndicatorColor(Color.parseColor("#990000"))
+                        .build()
+                        .show();
             }
         });
 
