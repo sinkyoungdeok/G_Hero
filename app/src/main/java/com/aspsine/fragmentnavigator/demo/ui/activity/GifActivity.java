eaa3d5259715ab1ed30f37e2c0f81edb41d5e632
sinkyoungdeok <tlsrudejr123@nate.com>
2020-12-12 19:33:29 +0900

    [ADD] (Gif 화면) 전달 받은 사진이 자유롭게 움직이고 확대축소가 되도록 구현

--- app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/activity/GifActivity.java
+++ app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/activity/GifActivity.java
@@ -8,20 +8,36 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.aspsine.fragmentnavigator.demo.R;
+import com.bumptech.glide.Glide;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import us.technerd.tnimageview.TNImageView;
 
 public class GifActivity extends AppCompatActivity {
     private String imageUriStr;
-    private ImageView img;
+    private ImageView faceImg;
+    private ImageView gifImg;
+    private TNImageView tnImageView;
+    private List<ImageView> imageViews = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gif);
-        img = (ImageView) findViewById(R.id.imageView6);
+        faceImg = (ImageView) findViewById(R.id.imageView6);
+        gifImg = (ImageView) findViewById(R.id.imageView4);
+        tnImageView = new TNImageView();
+
+        Glide.with(getApplicationContext()).load(R.mipmap.angry_no_face).into(gifImg);
+
+        imageViews.add(faceImg);
+        tnImageView.addListofImageViews(imageViews);
+        tnImageView.bringToFrontOnTouch(true);
         Intent intent = getIntent();
         imageUriStr = intent.getExtras().getString("imageUri");
-        img.setImageURI(Uri.parse(imageUriStr));
+        faceImg.setImageURI(Uri.parse(imageUriStr));
 
     }
 }
