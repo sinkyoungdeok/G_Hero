84efd0d9171eeaf26af837c9c206f744456fed93
sinkyoungdeok <tlsrudejr123@nate.com>
2020-12-12 18:23:40 +0900

    [ADD] (Gif 화면) 전달 받은 이미지 보여주기 완성

--- app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/activity/GifActivity.java
+++ app/src/main/java/com/aspsine/fragmentnavigator/demo/ui/activity/GifActivity.java
@@ -2,15 +2,26 @@ package com.aspsine.fragmentnavigator.demo.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.aspsine.fragmentnavigator.demo.R;
 
-public class GifActivity extends AppCompatActivity {
+import java.net.URI;
 
+public class GifActivity extends AppCompatActivity {
+    private String imageUriStr;
+    private ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gif);
+        img = (ImageView) findViewById(R.id.imageView6);
+        Intent intent = getIntent();
+        imageUriStr = intent.getExtras().getString("imageUri");
+        img.setImageURI(Uri.parse(imageUriStr));
+
     }
 }
